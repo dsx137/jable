d7dbb295dc306c695717b2602412d64c1d0285db
dsx137 <1419696375@qq.com>
2024-04-19 05:16:21 +0800

    judgement

--- src/test/java/com/github/dsx137/jable/multithreading/test.kt
+++ src/test/java/com/github/dsx137/jable/multithreading/test.kt
@@ -1,8 +1,11 @@
 package com.github.dsx137.jable.multithreading
 
+import com.github.dsx137.jable.extension.nor
+
 fun main() {
     val b1 = true
     val b2 = false
     val n1 = 1
     val n2 = 2
+    println(b1.nor(b2))
 }
