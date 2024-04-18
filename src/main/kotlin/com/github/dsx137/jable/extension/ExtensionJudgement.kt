64469f7f89a1b52ec4fd31d23cbbbc368fa5a2c6
dsx137 <70027572+dsx137@users.noreply.github.com>
2024-04-19 05:16:21 +0800

    judgement

--- src/main/kotlin/com/github/dsx137/jable/extension/ExtensionJudgement.kt
+++ src/main/kotlin/com/github/dsx137/jable/extension/ExtensionJudgement.kt
@@ -3,6 +3,6 @@ package com.github.dsx137.jable.extension
 fun Boolean.yes(predicate: () -> Unit): Boolean = this.also { if (it) predicate() }
 fun Boolean.no(predicate: () -> Unit): Boolean = this.also { if (!it) predicate() }
 
-fun Boolean.nor(boolean: Boolean): Boolean = !(this || boolean)
-fun Boolean.nand(boolean: Boolean): Boolean = !(this && boolean)
-fun Boolean.xnor(boolean: Boolean): Boolean = this == boolean
+infix fun Boolean.nor(boolean: Boolean): Boolean = !(this || boolean)
+infix fun Boolean.nand(boolean: Boolean): Boolean = !(this && boolean)
+infix fun Boolean.xnor(boolean: Boolean): Boolean = this == boolean
