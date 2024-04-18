76aec320d9ef875e05cb842a3c09a26ce3605807
dsx137 <70027572+dsx137@users.noreply.github.com>
2024-04-19 04:37:55 +0800

    fix

--- src/main/kotlin/com/github/dsx137/jable/extension/ExtensionJudgement.kt
+++ src/main/kotlin/com/github/dsx137/jable/extension/ExtensionJudgement.kt
@@ -7,11 +7,3 @@ fun Boolean?.yes(predicate: () -> Unit): Boolean {
 fun Boolean?.no(predicate: () -> Unit): Boolean {
     return this.takeIf { it == false || it == null }?.apply { predicate() } ?: false
 }
-
-fun Boolean?.yesStrict(predicate: () -> Unit): Boolean? {
-    return this.takeIf { it == true }?.apply { predicate() }
-}
-
-fun Boolean?.noStrict(predicate: () -> Unit): Boolean? {
-    return this.takeIf { it == false }?.apply { predicate() }
-}
