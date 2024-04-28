68b1c60ca333a3b81738d04600a7291524c35a61
dsx137 <1419696375@qq.com>
2024-04-28 17:28:23 +0800

    LatchDispatcher

--- src/test/java/com/github/dsx137/jable/multithreading/MainTest.java
+++ src/test/java/com/github/dsx137/jable/multithreading/MainTest.java
@@ -1,12 +1,39 @@
 package com.github.dsx137.jable.multithreading;
 
-import com.github.dsx137.jable.random.RandomNickname;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.Thread.sleep;
 
 public class MainTest {
 
     public static void main(String[] args) {
-        RandomNickname randomNickname = new RandomNickname();
-        System.out.println(randomNickname.generate(100));
+        LatchDispatcher<String> bus = new LatchDispatcher<>(1, 500, TimeUnit.MILLISECONDS);
+
+        String uuid = UUID.randomUUID().toString();
+
+        Thread t1 = new Thread(() -> {
+            try {
+                sleep(6000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            bus.countDown(uuid);
+        });
+
+        Thread t2 = new Thread(() -> {
+            boolean isSuccessful = bus.await(uuid);
+
+            if (isSuccessful) {
+                System.out.println("hello");
+            } else {
+                System.out.println("failed");
+            }
+        });
+
+        t1.start();
+        t2.start();
+
     }
 
 }
